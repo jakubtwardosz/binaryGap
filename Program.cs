22f078a8d183780d2baa0d31add491d0b5c15a30
Jakub Twardosz <jakubtwardosz@protonmail.com>
2024-03-27 23:14:58 +0100

    Improved calculation of longest gap

--- Program.cs
+++ Program.cs
@@ -3,26 +3,42 @@
 int number = int.Parse(Console.ReadLine());
 int currentGapLength = 0;
 int maxGapLength = 0;
+bool gapStarted = false;
+string binaryRepresentation = string.Empty;
 
 while (number > 0)
 {
     int result = number % 2;
+    binaryRepresentation = result.ToString() + binaryRepresentation;
 
     if (result == 0)
     {
-        currentGapLength++;
+        if (gapStarted)
+        {
+            currentGapLength++;
+        }
     }
     else
     {
-        if (currentGapLength > maxGapLength)
+        if (!gapStarted)
         {
-            maxGapLength = currentGapLength;
+            gapStarted = true;
+        }
+        else
+        {
+            if (currentGapLength > maxGapLength)
+            {
+                maxGapLength = currentGapLength;
+            }
+            currentGapLength = 0;
         }
 
-        currentGapLength = 0;
+
     }
 
     number = number / 2;
 }
 
+
+Console.WriteLine("Binary: " + binaryRepresentation);
 Console.WriteLine("Longest binary gap: " + maxGapLength);
--- obj/Debug/net8.0/binaryGap.AssemblyInfo.cs
+++ obj/Debug/net8.0/binaryGap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("binaryGap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab4eae7e6b8487a6797092e269a56b32a569c32")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256acc6ba4186849f4d620ab4f769333c1796c97")]
 [assembly: System.Reflection.AssemblyProductAttribute("binaryGap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("binaryGap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
